248f7e5e136421635ff3baac53842f34fe6cb121
seanwes <sean@example.com>
2024-11-17 19:50:09 +0800

    login added

--- index.php
+++ index.php
@@ -3,12 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" rel="stylesheet">
-    <title>Document</title>
+    <title>Login and Register</title>
 </head>
 <body>
+    
+    <form action="register.php" method="post">
+        <h1>Register</h1>
+        <input type="email" name="email" placeholder="Enter email" required>
+        <label for="email">Email:</label>
+        <input type="text" name="username" placeholder="Enter username" required>
+        <label for="username">Username:</label> 
+        <input type="password" name="password" placeholder="Enter password" required>
+        <label for="password">Password:</label>
+        <button type="submit" name= "signUp">Sign Up</button>
+    </form>
 
-
+    <form action="register.php" method="post">
+        <h1>Login</h1>
+        <input type="text" name="email" placeholder="Enter email" required>
+        <label for="username">Email:</label> 
+        <input type="password" name="password" placeholder="Enter password" required>
+        <label for="password">Password:</label>
+        <button type="submit" name= "login">Login</button>
+    </form>
 
 </body>
 </html>
