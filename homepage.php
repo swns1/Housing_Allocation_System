cf55d5f6b2c388d60e93053507badd59bf64ae07
jvyana <juliencecastillo@gmail.com>
2024-11-23 22:11:50 +0800

    changed search btn to logout btn

---
+++ homepage.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Housing Allocation</title>
+  <link rel="stylesheet" href="homepage-style.css">
+</head>
+<body>
+
+  <div class="main">
+    <div class="navbar">
+      <div class="icon">
+        <h2 class="logo">Neighborly</h2>
+      </div>
+
+      <div class="menu">
+        <ul>
+          <li><a href="#">HOME</a></li>
+          <li><a href="#">MY PROFILE</a></li>
+          <li><a href="searching.php">HOUSING OFFERS</a></li>
+          <li><a href="#">ABOUT</a></li>
+        </ul>
+      </div>
+
+      <div class="logout">  <a href="#"> <button class="btn">LOGOUT</button></a>
+      </div>
+
+    </div>
+    <div class="content">
+      <h1><br><span>Housing Allocation System</span></h1>
+      <p class="par">"Empowering cities and communities with technology to manage affordable housing, aligned with <br><b>Sustainable Development Goal 11</b>: Building inclusive, safe, and sustainable urban spaces."</p>
+
+      <button class="cn"><a href="#"><b>Apply Now</b></a></button>
+
+    </div>
+  <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
+</body>
+</html>
